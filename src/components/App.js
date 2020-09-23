f97b2a35f0f0ce07db8a497d68ba7bf466a9f470
KIRUBAKARAN MUTHUPALANI <kiru@KIRUBAKARANsMBP.home>
2020-09-23 20:41:28 +0100

    scoreboard components

---
+++ src/components/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player';
+import AddPlayer from './addPlayer';
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: 'Guil',
+        score: 0,
+        id: 1,
+      },
+      {
+        name: 'Treasure',
+        score: 0,
+        id: 2,
+      },
+      {
+        name: 'Ashley',
+        score: 0,
+        id: 3,
+      },
+      {
+        name: 'James',
+        score: 0,
+        id: 4,
+      },
+    ],
+  };
+
+  // player id counter
+  prevPlayerId = 4;
+
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => ({
+      score: (prevState.players[index].score += delta),
+    }));
+  };
+
+  handleAddPlayer = (name) => {
+    this.setState((prevState) => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: (this.prevPlayerId += 1),
+          },
+        ],
+      };
+    });
+  };
+
+  handleRemovePlayer = (id) => {
+    this.setState((prevState) => {
+      return {
+        players: prevState.players.filter((p) => p.id !== id),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className='scoreboard'>
+        <Header title='Scoreboard' players={this.state.players} />
+
+        {/* Players list */}
+        {this.state.players.map((player, index) => (
+          <Player
+            name={player.name}
+            id={player.id}
+            index={index}
+            score={player.score}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+            changeScore={this.handleScoreChange}
+          />
+        ))}
+
+        <AddPlayer addPlayer={this.handleAddPlayer} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/addPlayer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class AddPlayer extends Component {
+  state = {
+    value: '',
+  };
+
+  handleValueChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addPlayer(this.state.value);
+    this.setState({
+      value: '',
+    });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          value={this.state.value}
+          onChange={this.handleValueChange}
+          placeholder="Enter Player's name"
+        />
+        <input
+          type='submit'
+          value='Add Player'
+          onClick={this.handleAddPlayer}
+        />
+      </form>
+    );
+  }
+}
+
+export default AddPlayer;
